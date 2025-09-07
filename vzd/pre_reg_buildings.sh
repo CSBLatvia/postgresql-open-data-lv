02c9baedda6fc2124298fa6c115306226691c21c
Dāvis Kļaviņš <68686450+Davis-Klavins@users.noreply.github.com>
2025-09-07 09:54:19 +0300

    Update pre_reg_buildings.sh
    Mainīta saite.

--- vzd/pre_reg_buildings.sh
+++ vzd/pre_reg_buildings.sh
@@ -12,7 +12,6 @@ do
   curl "$FILE" -o ${FILE##*/}
 done
 
-mv *_pirmreg_buves.xlsx pirmreg_buves.xlsx
 rm *.csv
 rm *.json
 psql -U scheduler -d spatial -w -c "CALL vzd.nivkis_building_pre_reg_proc()"
